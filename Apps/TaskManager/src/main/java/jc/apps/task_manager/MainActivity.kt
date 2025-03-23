1d4f16aaf03183b016e2c17c83112a800f59c2d1
HoiCham <p.n.thang0101@gmail.com>
2025-03-24 01:20:18 +0700

    :package: Introduce Navigation with NavGraph and NavigationActions

--- Apps/TaskManager/src/main/java/jc/apps/task_manager/MainActivity.kt
+++ Apps/TaskManager/src/main/java/jc/apps/task_manager/MainActivity.kt
@@ -4,24 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import jc.apps.task_manager.ui.NavGraph
 import modules.core.theme.JetpackComposeTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             JetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = "Hello Android!", modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
         }
     }
---
+++ Apps/TaskManager/src/main/java/jc/apps/task_manager/ui/NavGraph.kt
@@ -0,0 +1,26 @@
+package jc.apps.task_manager.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph(
+    modifier: Modifier = Modifier,
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = AppDestinations.HOME_ROUTE,
+        modifier = modifier
+    ) {
+        composable(
+            route = AppDestinations.HOME_ROUTE,
+        ) { navBackStackEntry ->
+
+        }
+    }
+}
