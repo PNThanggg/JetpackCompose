862ade9a77e22f2d9c75087019db84c97088b56b
HoiCham <p.n.thang0101@gmail.com>
2025-03-26 03:39:10 +0700

    :sparkles: Introduce SplashScreen and navigate to Home after delay

---
+++ Apps/LoLData/src/main/java/jc/apps/lol/presentation/splash/SplashScreen.kt
@@ -0,0 +1,45 @@
+package jc.apps.lol.presentation.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import jc.apps.lol.R
+import jc.apps.lol.presentation.Home
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    navController: NavController
+) {
+    LaunchedEffect(Unit) {
+        delay(500)
+
+        navController.navigate(Home) {
+            popUpTo(navController.graph.startDestinationId) { inclusive = true }
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            modifier = Modifier
+                .width(LocalConfiguration.current.screenWidthDp.dp * 0.5F)
+                .aspectRatio(1f),
+            painter = painterResource(id = R.drawable.ic_logo_splash),
+            contentDescription = stringResource(id = R.string.app_name)
+        )
+    }
+}
