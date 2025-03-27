eb1e6756b3043f57633f4afae4953744d59d62bc
HoiCham <p.n.thang0101@gmail.com>
2025-03-27 16:55:36 +0700

    :recycle: Refactor ChampionDetailsViewModel to load champion data by name and add logging.

--- Apps/LoLData/src/main/java/jc/apps/lol/presentation/detail/ChampionDetailsViewModel.kt
+++ Apps/LoLData/src/main/java/jc/apps/lol/presentation/detail/ChampionDetailsViewModel.kt
@@ -2,7 +2,6 @@ package jc.apps.lol.presentation.detail
 
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,20 +12,20 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
+private const val TAG = "ChampionDetails"
+
 @HiltViewModel
 class ChampionDetailsViewModel @Inject constructor(
     private val getChampionByNameUseCase: GetChampionByNameUseCase,
-    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     var champion = mutableStateOf<ChampionModel?>(null)
 
-    init {
-        val name: String = savedStateHandle["name"] ?: "NULL"
-
+    fun loadChampion(name: String) {
+        Log.d(TAG, "loadChampion(): $name")
         viewModelScope.launch {
             val result = withContext(Dispatchers.IO) { getChampionByNameUseCase.run(name) }
             result.fold(fnL = { failure ->
-                Log.d("TAG", "ChampionDetailsViewModel: ${failure.message}")
+                Log.d(TAG, "ChampionDetailsViewModel: ${failure.message}")
             }, fnR = { response ->
                 champion.value = response.champion.values.firstOrNull()
             })
