cb29b192014d114bb52b8c8f16bd3ad140c082e7
HoiCham <p.n.thang0101@gmail.com>
2025-03-31 11:07:38 +0700

    :package: Introduce BankPick App Module with Initial Setup
    -   :heavy_plus_sign: Added the `BankPick` app module to the project.
    -   :page_facing_up: Created `AndroidManifest.xml` to define the application's components, including the main activity.
    -   :hammer: Created `build.gradle.kts` to configure the module, including Android configurations and dependencies.
    -   :file_folder: Added resource directories for `mipmap`, `values`, and `drawable`.
    -   :art: Added launcher icons (`ic_launcher`) and background images.
    -   :art: Added Bankpick logo `bankpick_logo.png`
    -   :pencil: Added `strings.xml` and `colors.xml` for resource management.
    -   :art: Added `themes.xml` for UI theming.
    -   :file_folder: Added test folder and unit test class
    -   :heavy_plus_sign: Added initial Compose UI code in `MainActivity.kt` and `SplashScreen.kt`.
    -   :memo: Added `proguard-rules.pro` file.
    -   :memo: Added `README.md` file.
    -   :file_folder: Added `images` folder with UI design screenshots.
    -   :memo: Added `.gitignore` file.
    -   :heavy_plus_sign: Updated `settings.gradle.kts` to include the new `BankPick` module.

---
+++ Apps/BankPick/src/main/java/jc/app/bank_pick/MainActivity.kt
@@ -0,0 +1,19 @@
+package jc.app.bank_pick
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import modules.core.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeTheme {
+                SplashScreen()
+            }
+        }
+    }
+}
---
+++ Apps/BankPick/src/main/java/jc/app/bank_pick/SplashScreen.kt
@@ -0,0 +1,45 @@
+package jc.app.bank_pick
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SplashScreen() {
+    Scaffold { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier.size(48.dp),
+                    painter = painterResource(id = R.drawable.bankpick_logo),
+                    contentDescription = stringResource(R.string.app_name),
+                )
+
+                Text(
+                    text = stringResource(R.string.app_name),
+                    modifier = Modifier.padding(top = 16.dp),
+                    style = MaterialTheme.typography.headlineMedium,
+                )
+            }
+        }
+    }
+}
