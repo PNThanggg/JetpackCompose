3d701438b58966b4c48ec1a96bfb391887d8ca34
HoiCham <p.n.thang0101@gmail.com>
2025-03-31 10:49:30 +0700

    :package: Implement UI components for HomeScreen

--- Apps/BankingUI/src/main/java/jc/apps/banking/screen/HomeScreen.kt
+++ Apps/BankingUI/src/main/java/jc/apps/banking/screen/HomeScreen.kt
@@ -1,12 +1,19 @@
 package jc.apps.banking.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import jc.apps.banking.widget.BottomNavigationBar
+import jc.apps.banking.widget.CardsSection
+import jc.apps.banking.widget.CurrenciesSection
+import jc.apps.banking.widget.FinanceSection
+import jc.apps.banking.widget.WalletSection
 
 @Composable
 fun HomeScreen() {
@@ -19,7 +26,15 @@ fun HomeScreen() {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
+            WalletSection()
 
+            CardsSection()
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            FinanceSection()
+
+            CurrenciesSection()
         }
     }
 }
