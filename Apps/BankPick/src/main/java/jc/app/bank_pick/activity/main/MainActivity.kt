a2730dd9eb2bc1e0d94a56b42acc5b1ff925c1a3
HoiCham <p.n.thang0101@gmail.com>
2025-04-01 10:33:52 +0700

    :lipstick: Update MainActivity and remove unused activities and imports

--- Apps/BankPick/src/main/java/jc/app/bank_pick/activity/main/MainActivity.kt
+++ Apps/BankPick/src/main/java/jc/app/bank_pick/activity/main/MainActivity.kt
@@ -1,17 +1,11 @@
 package jc.app.bank_pick.activity.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import jc.app.bank_pick.activity.authentication.AuthenticationActivity
-import jc.app.bank_pick.activity.intro.IntroActivity
-import jc.app.bank_pick.activity.intro.IntroScreen
 import jc.app.bank_pick.activity.main.main.MainScreen
-import kotlinx.coroutines.launch
 import modules.core.theme.JetpackComposeTheme
 
 @AndroidEntryPoint
