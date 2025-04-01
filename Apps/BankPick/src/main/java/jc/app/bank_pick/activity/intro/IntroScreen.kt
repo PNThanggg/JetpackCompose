29cf36c637a47f798998f223dbba839457a00b77
HoiCham <p.n.thang0101@gmail.com>
2025-04-01 09:56:44 +0700

    :memo: Introduce Next Button and page navigation in IntroScreen.

--- Apps/BankPick/src/main/java/jc/app/bank_pick/activity/intro/IntroScreen.kt
+++ Apps/BankPick/src/main/java/jc/app/bank_pick/activity/intro/IntroScreen.kt
@@ -3,14 +3,24 @@ package jc.app.bank_pick.activity.intro
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,9 +34,7 @@ import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun IntroScreen(
-    modifier: Modifier = Modifier,
-) {
+fun IntroScreen() {
     val pages = listOf(
         OnBoardingPage.First, OnBoardingPage.Second, OnBoardingPage.Third
     )
@@ -36,10 +44,55 @@ fun IntroScreen(
         },
     )
 
-    HorizontalPager(
-        state = pagerState, verticalAlignment = Alignment.Top
-    ) { position ->
-        PagerScreen(onBoardingPage = pages[position])
+    var currentIndex by remember { mutableIntStateOf(0) }
+
+    LaunchedEffect(currentIndex) {
+        pagerState.animateScrollToPage(currentIndex)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Spacer(modifier = Modifier.weight(1F))
+
+        HorizontalPager(
+            state = pagerState,
+            verticalAlignment = Alignment.Top,
+            userScrollEnabled = false,
+        ) { position ->
+            PagerScreen(onBoardingPage = pages[position])
+        }
+
+        Spacer(modifier = Modifier.weight(1F))
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            onClick = {
+                if (currentIndex < pages.size - 1) {
+                    currentIndex += 1
+                } else {
+
+                }
+            },
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors().copy(
+                containerColor = Color(0xFF0066FF)
+            ),
+        ) {
+            Text(
+                text = "Next", style = MaterialTheme.typography.bodyMedium.copy(
+                    color = Color.White,
+                    fontWeight = FontWeight.W500,
+                    fontSize = TextUnit(16F, TextUnitType.Sp),
+                    lineHeight = TextUnit(24F, TextUnitType.Sp),
+                    textAlign = TextAlign.Center,
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.height(60.dp))
     }
 }
 
