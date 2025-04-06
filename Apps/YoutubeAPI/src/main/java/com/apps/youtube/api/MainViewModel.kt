e733c4fc8858a7e9f51d2fcc939bff31f7dd223b
HoiCham <p.n.thang0101@gmail.com>
2025-04-07 00:06:48 +0700

    :white_check_mark: Implement `fetchVideos` in `MainViewModel` to retrieve videos using `GetVideosWithDetailsUseCase` and update LiveData.

--- Apps/YoutubeAPI/src/main/java/com/apps/youtube/api/MainViewModel.kt
+++ Apps/YoutubeAPI/src/main/java/com/apps/youtube/api/MainViewModel.kt
@@ -1,8 +1,34 @@
 package com.apps.youtube.api
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.apps.youtube.api.data.models.VideoListResponse
+import com.apps.youtube.api.domain.usecase.GetVideosWithDetailsParams
+import com.apps.youtube.api.domain.usecase.GetVideosWithDetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {}
+class MainViewModel @Inject constructor(
+    private val getVideosWithDetailsUseCase: GetVideosWithDetailsUseCase
+) : ViewModel() {
+    private val _videos = MutableLiveData<VideoListResponse>()
+    val videos: LiveData<VideoListResponse> get() = _videos
+
+    fun fetchVideos(channelId: String, pageToken: String? = null) {
+        val params = GetVideosWithDetailsParams(channelId, pageToken)
+        viewModelScope.launch {
+            val result = withContext(Dispatchers.IO) { getVideosWithDetailsUseCase.run(params) }
+            result.fold(fnL = { failure ->
+
+            }, fnR = { response ->
+                _videos.value = response
+            })
+        }
+    }
+}
