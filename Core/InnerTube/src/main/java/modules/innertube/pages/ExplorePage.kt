a7d63b41c0cf68097e1552fa26c46603a0fbada5
HoiCham <p.n.thang0101@gmail.com>
2025-04-07 23:42:48 +0700

    :sparkles: Introduce ExplorePage data class for new release albums and mood/genres.

---
+++ Core/InnerTube/src/main/java/modules/innertube/pages/ExplorePage.kt
@@ -0,0 +1,8 @@
+package modules.innertube.pages
+
+import modules.innertube.models.AlbumItem
+
+data class ExplorePage(
+    val newReleaseAlbums: List<AlbumItem>,
+    val moodAndGenres: List<MoodAndGenres.Item>,
+)
