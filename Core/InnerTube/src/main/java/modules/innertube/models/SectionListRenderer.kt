ffb242d777147efa49f04839dccf315e83da0767
HoiCham <p.n.thang0101@gmail.com>
2025-04-07 19:26:38 +0700

    :heavy_plus_sign: Introduce SectionListRenderer model for parsing section list data.

---
+++ Core/InnerTube/src/main/java/modules/innertube/models/SectionListRenderer.kt
@@ -0,0 +1,39 @@
+package modules.innertube.models
+
+import com.google.gson.annotations.SerializedName
+
+data class SectionListRenderer(
+    @SerializedName("header") val header: Header? = null,
+    @SerializedName("contents") val contents: List<Content>? = null,
+    @SerializedName("continuations") val continuations: List<Continuation>? = null
+) {
+    data class Header(
+        @SerializedName("chipCloudRenderer") val chipCloudRenderer: ChipCloudRenderer? = null
+    ) {
+        data class ChipCloudRenderer(
+            @SerializedName("chips") val chips: List<Chip>
+        ) {
+            data class Chip(
+                @SerializedName("chipCloudChipRenderer") val chipCloudChipRenderer: ChipCloudChipRenderer
+            ) {
+                data class ChipCloudChipRenderer(
+                    @SerializedName("isSelected") val isSelected: Boolean,
+                    @SerializedName("navigationEndpoint") val navigationEndpoint: NavigationEndpoint,
+                    @SerializedName("text") val text: Runs? = null,
+                    @SerializedName("uniqueId") val uniqueId: String? = null
+                )
+            }
+        }
+    }
+
+    data class Content(
+        @SerializedName("musicImmersiveCarouselShelfRenderer") val musicCarouselShelfRenderer: MusicCarouselShelfRenderer? = null,
+        @SerializedName("musicShelfRenderer") val musicShelfRenderer: MusicShelfRenderer? = null,
+        @SerializedName("musicCardShelfRenderer") val musicCardShelfRenderer: MusicCardShelfRenderer? = null,
+        @SerializedName("musicPlaylistShelfRenderer") val musicPlaylistShelfRenderer: MusicPlaylistShelfRenderer? = null,
+        @SerializedName("musicDescriptionShelfRenderer") val musicDescriptionShelfRenderer: MusicDescriptionShelfRenderer? = null,
+        @SerializedName("gridRenderer") val gridRenderer: GridRenderer? = null,
+        @SerializedName("musicResponsiveHeaderRenderer") val musicResponsiveHeaderRenderer: BrowseResponse.Header.MusicHeaderRenderer? = null,
+        @SerializedName("musicEditablePlaylistDetailHeaderRenderer") val musicEditablePlaylistDetailHeaderRenderer: BrowseResponse.Header.MusicEditablePlaylistDetailHeaderRenderer? = null
+    )
+}
