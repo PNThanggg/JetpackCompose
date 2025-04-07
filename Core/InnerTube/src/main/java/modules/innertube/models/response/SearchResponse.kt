2240e2aba276dbe978eea86d911e0303b49d4b46
HoiCham <p.n.thang0101@gmail.com>
2025-04-07 19:01:42 +0700

    :heavy_plus_sign: Introduce SearchResponse data class.

---
+++ Core/InnerTube/src/main/java/modules/innertube/models/response/SearchResponse.kt
@@ -0,0 +1,26 @@
+package modules.innertube.models.response
+
+import com.google.gson.annotations.SerializedName
+import modules.innertube.models.Tabs
+
+data class SearchResponse(
+    @SerializedName("contents") val contents: Contents?,
+    @SerializedName("continuationContents") val continuationContents: ContinuationContents?,
+) {
+    data class Contents(
+        @SerializedName("tabbedSearchResultsRenderer") val tabbedSearchResultsRenderer: Tabs?,
+    )
+
+    data class ContinuationContents(
+        @SerializedName("musicShelfContinuation") val musicShelfContinuation: MusicShelfContinuation,
+    ) {
+        data class MusicShelfContinuation(
+            @SerializedName("contents") val contents: List<Content>,
+            @SerializedName("continuations") val continuations: List<Continuation>?,
+        ) {
+            data class Content(
+                @SerializedName("musicResponsiveListItemRenderer") val musicResponsiveListItemRenderer: MusicResponsiveListItemRenderer,
+            )
+        }
+    }
+}
