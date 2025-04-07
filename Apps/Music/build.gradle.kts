eb58b4988fc04ebe2a34421d7fc0594474133114
HoiCham <p.n.thang0101@gmail.com>
2025-04-07 09:37:26 +0700

    :heavy_plus_sign: Add necessary plugins and dependencies for Music app module

--- Apps/Music/build.gradle.kts
+++ Apps/Music/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.dagger.hilt)
 }
 
 android {
@@ -52,6 +54,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.timber)
+
+    implementation(libs.gson)
+    implementation(libs.okhttp3)
+    implementation(libs.okhttp3.logging.interceptor)
+    implementation(libs.retrofit2)
+    implementation(libs.retrofit2.converter.gson)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
     implementation(project(":Core:LrcLib"))
 
