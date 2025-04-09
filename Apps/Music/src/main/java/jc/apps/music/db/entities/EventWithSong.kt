fd53ec60fc51f3e02a8368f539af57b2858695aa
HoiCham <p.n.thang0101@gmail.com>
2025-04-09 23:48:20 +0700

    :construction: Introduce EventWithSong entity for Room database relations.

---
+++ Apps/Music/src/main/java/jc/apps/music/db/entities/EventWithSong.kt
@@ -0,0 +1,14 @@
+package jc.apps.music.db.entities
+
+import androidx.compose.runtime.Immutable
+import androidx.room.Embedded
+import androidx.room.Relation
+
+@Immutable
+data class EventWithSong(
+    @Embedded val event: Event,
+    @Relation(
+        entity = SongEntity::class, parentColumn = "songId", entityColumn = "id"
+    )
+    val song: Song,
+)
