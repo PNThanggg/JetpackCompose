0d52291d85e75f403443c8a4a3481e5cc9603f27
HoiCham <p.n.thang0101@gmail.com>
2025-04-09 23:33:36 +0700

    :construction: Introduce SimilarRecommendation model with LocalItem and YTItem

--- Apps/Music/src/main/java/jc/apps/music/models/SimilarRecommendation.kt
+++ Apps/Music/src/main/java/jc/apps/music/models/SimilarRecommendation.kt
@@ -1,5 +1,8 @@
 package jc.apps.music.models
 
+import jc.apps.music.db.entities.LocalItem
+import modules.innertube.models.YTItem
+
 data class SimilarRecommendation(
     val title: LocalItem,
     val items: List<YTItem>,
