981e26e3d5c00a3c7b4b1be983ded5de696d0798
HoiCham <p.n.thang0101@gmail.com>
2025-04-10 07:49:36 +0700

    :sparkles: Introduce YouTubeSubtitleLyricsProvider for fetching lyrics from YouTube subtitles.

---
+++ Apps/Music/src/main/java/jc/apps/music/lyrics/YouTubeSubtitleLyricsProvider.kt
@@ -0,0 +1,18 @@
+package jc.apps.music.lyrics
+
+import android.content.Context
+import modules.innertube.YouTube
+
+object YouTubeSubtitleLyricsProvider : LyricsProvider {
+    override val name = "YouTube Subtitle"
+    override fun isEnabled(context: Context) = true
+    override suspend fun getLyrics(
+        id: String, title: String, artist: String, duration: Int
+    ): Result<String> = YouTube.transcript(id)
+
+    override suspend fun getAllLyrics(
+        id: String, title: String, artist: String, duration: Int
+    ): Result<List<String>> = runCatching {
+        emptyList<String>()
+    }
+}
