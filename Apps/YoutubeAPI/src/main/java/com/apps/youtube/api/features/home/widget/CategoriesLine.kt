544e208e47400ac816737c5ea6a0b324cfdf4ef6
HoiCham <p.n.thang0101@gmail.com>
2025-04-10 11:20:56 +0700

    :lipstick: Improve CategoriesLine UI with an "Explore" text and padding.

--- Apps/YoutubeAPI/src/main/java/com/apps/youtube/api/features/home/widget/CategoriesLine.kt
+++ Apps/YoutubeAPI/src/main/java/com/apps/youtube/api/features/home/widget/CategoriesLine.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -15,13 +18,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.apps.youtube.api.R
 import com.apps.youtube.api.data.models.Categories
 
+@PreviewLightDark()
 @Composable
 fun CategoriesLine(
     openDrawer: () -> Unit = {},
@@ -35,7 +41,7 @@ fun CategoriesLine(
         modifier = Modifier.fillMaxWidth()
     ) {
         items(categories.size) { index ->
-            if (index == 0) Box(
+            if (index == 0) Row(
                 modifier = Modifier
                     .padding(
                         start = 12.dp,
@@ -47,6 +53,7 @@ fun CategoriesLine(
                     .clickable {
                         openDrawer.invoke()
                     },
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_discover),
@@ -54,6 +61,18 @@ fun CategoriesLine(
                     tint = if (isDarkTheme) Color.White else Color.Black,
                     modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
                 )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = "Explore", color = if (isDarkTheme) {
+                        Color.White
+                    } else {
+                        Color.Black
+                    }
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
             }
 
             Box(
