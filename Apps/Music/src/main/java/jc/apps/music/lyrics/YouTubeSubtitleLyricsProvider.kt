8500e2024d4cd36a5681620cf4fb768c41d340af
HoiCham <p.n.thang0101@gmail.com>
2025-04-10 08:27:24 +0700

    :white_check_mark: Enable YouTube Subtitle Lyrics Provider by default.

--- Apps/Music/src/main/java/jc/apps/music/lyrics/YouTubeSubtitleLyricsProvider.kt
+++ Apps/Music/src/main/java/jc/apps/music/lyrics/YouTubeSubtitleLyricsProvider.kt
@@ -5,7 +5,9 @@ import modules.innertube.YouTube
 
 object YouTubeSubtitleLyricsProvider : LyricsProvider {
     override val name = "YouTube Subtitle"
+
     override fun isEnabled(context: Context) = true
+
     override suspend fun getLyrics(
         id: String, title: String, artist: String, duration: Int
     ): Result<String> = YouTube.transcript(id)
