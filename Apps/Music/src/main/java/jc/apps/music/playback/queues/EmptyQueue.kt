7cb6d2333b53e9898e34e26369cec468c0ec822b
HoiCham <p.n.thang0101@gmail.com>
2025-04-10 08:04:29 +0700

    :package: Introduce EmptyQueue as a default implementation of the Queue interface.

---
+++ Apps/Music/src/main/java/jc/apps/music/playback/queues/EmptyQueue.kt
@@ -0,0 +1,14 @@
+package jc.apps.music.playback.queues
+
+import androidx.media3.common.MediaItem
+import jc.apps.music.models.MediaMetadata
+
+object EmptyQueue : Queue {
+    override val preloadItem: MediaMetadata? = null
+
+    override suspend fun getInitialStatus() = Queue.Status(null, emptyList(), -1)
+
+    override fun hasNextPage() = false
+
+    override suspend fun nextPage() = emptyList<MediaItem>()
+}
